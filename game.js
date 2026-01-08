a9bcf8c46f68dca123f66f256c8a2e13243b45c7
gmeshw-stack <gmeshw@apps.gmes.ntpc.edu.tw>
2026-01-08 23:33:20 +0800

    Add files via upload

--- game.js
+++ game.js
@@ -15,11 +15,11 @@ resetBtn.onclick = resetGame;
 
 /* 像素級定位（對齊你的餐盤圖） */
 const positions = {
-  dairy: { top: "10px", left: "10px", width: "78px", height: "80px" },
-  fruit: { top: "95px", left: "60px", width: "58px", height: "208px" },
-  vegetable: { top: "12px", left: "126px", width: "100px", height: "209px" },
-  protein: { top: "92px", left: "229px", width: "181px", height: "86px" },
-  grain: { top: "196px", left: "229px", width: "181px", height: "90px" }
+  dairy: { top: "10px", left: "10px", width: "88px", height: "80px" },
+  fruit: { top: "100px", left: "60px", width: "58px", height: "200px" },
+  vegetable: { top: "95px", left: "136px", width: "95px", height: "209px" },
+  protein: { top: "92px", left: "234px", width: "176px", height: "81px" },
+  grain: { top: "201px", left: "234px", width: "176px", height: "95px" }
 };
 
 function startGame() {
